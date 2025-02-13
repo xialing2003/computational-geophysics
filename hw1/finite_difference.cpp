86c01414495d70a9c1f6cffeb8ce0f38e9eb1966
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2025-02-13 15:54:37 -0500

    update

--- hw1/finite_difference.cpp
+++ hw1/finite_difference.cpp
@@ -9,9 +9,13 @@ int main()
     //initialization
     for(int i = 0; i<=1000; i++){
         u[0][i] = exp(-0.1*pow(dx*i-50, 2));
+        u[1][i] = exp(-0.1*pow(dx*i-50, 2));
     }
-    for(int n = 2; n <= 500; n++){
-        for(int i = 0; i <= 1000; i++){
+    cout << "1" << endl;
+    for(int n = 1; n < 500-1; n++){
+        u[n+1][0] = 0;
+        u[n+1][1000] = 0;
+        for(int i = 1; i < 1000; i++){
             u[n+1][i] = pow(c*dt/dx, 2) * (u[n][i+1] - 2*u[n][i] + u[n][i-1]) + 2*u[n][i] - u[n-1][i];
         }
     }
